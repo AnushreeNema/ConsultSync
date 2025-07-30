fa9c9cb35ea1f5194d76822eadbbe7b5a4d1cf06
Anushree Nema <anushreenema624@gmail.com>
2025-07-30 13:03:47 +0530

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default async function HomePage() {
       {/* SeenPatients will show at the top always */}
 
       {/* Main Dashboard UI */}
-      <DashboardLayout />
+      <DashboardLayout user={user} />
     </div>
   );
 }
